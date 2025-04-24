50c17610366e6e42be4a5041bc7e840e69eb6127
Incognitobotz <177184463+Incognitobotz@users.noreply.github.com>
2025-04-24 21:31:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -85,8 +85,8 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/dreamcinezone')  # Ho
 # Link Shortener Configuration
 # ============================
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'linkmonetizer.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'c063f52de487e95215a3e75ed967431459313664')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'linkshortify.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '6292c8c598153b0ca474654c78ce8b088304752c')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/JpMovies_Official/104')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 
