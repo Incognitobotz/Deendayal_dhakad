79ffbc37a494b2f5d92d926a3f5c419e446c4c85
Incognitobotz <177184463+Incognitobotz@users.noreply.github.com>
2025-07-08 20:19:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -85,8 +85,8 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/JpMovies_Official/104
 # Link Shortener Configuration
 # ============================
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'linkcents.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '668a8fc02c5c5893b5449c87e4f012aa4b1f48b0')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'smallshorts.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '44483cf111781546df41ef00bbdc5b7d638b8e90')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/JpMovies_Official/104')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 
